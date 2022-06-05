ff8f58fe422f5715eea46ab1c4f0fc1cdb90b313
Tim Stadtmann <timstadtmann@gmail.com>
2022-06-05 12:47:53 +0200

    Add multiple clock domains

--- src/hdl/top.v
+++ src/hdl/top.v
@@ -6,16 +6,23 @@ module top(
   output reg [3:0] led
 );
 
+wire clk_50MHz, clk_200MHz;
+clk_wiz(
+  .clk_in(clk_100MHz),
+  .clk_50MHz(clk_50MHz),
+  .clk_200MHz(clk_200MHz)
+);
+
 wire [6:0] duty_cycle = sw[0] ? 7'd50 : 7'd0;
 wire       pwm;
 pwm(
-  .clk(clk_100MHz),
+  .clk(clk_50MHz),
   .rst(1'b0),
   .duty_cycle(duty_cycle),
   .driver(pwm)
 );
 
-always@(posedge clk_100MHz) begin
+always@(posedge clk_200MHz) begin
 led[0] <= pwm;
 led[1] <= pwm;
 led[2] <= pwm;
